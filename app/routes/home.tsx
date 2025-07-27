59d1dd58398f1aaa40063fc5569a2e5b9678dbad
MONU MANDAL <monumandel3@gmail.com>
2025-07-27 18:19:18 +0530

    added a image to the background of home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,7 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <section className="main-section">
         <div className="page-heading">
           <h1>Track Your Applications & Resume Ratings</h1>
