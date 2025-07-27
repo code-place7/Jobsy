9f454693a5c8d8861ee38f42ad1a7481ce77b51c
MONU MANDAL <monumandel3@gmail.com>
2025-07-27 18:17:00 +0530

    remove existing welcome folder and create home page header

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Jobsy" },
+    { name: "description", content: "Welcome to Your Resume Buddy!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main>
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submission and check AI-powered feddback</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
