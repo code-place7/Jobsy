37188c262cc25e9d653e29e30fbf9b1e0342b26a
MONU MANDAL <monumandel3@gmail.com>
2025-07-29 19:42:41 +0530

    added resume image to the home page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -28,6 +28,16 @@ const ResumeCard = ({
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
+
+      <div className="gradient-border animate-in fade-in duration-1000">
+        <div className="w-full h-full">
+          <img
+            src={imagePath}
+            alt="resume"
+            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+          />
+        </div>
+      </div>
     </Link>
   );
 };
