75d4ac818b78bff89f327e79b7482bff04146219
MONU MANDAL <monumandel3@gmail.com>
2025-07-30 20:04:27 +0530

    implement autentication without using useState hook as usual way  instead use zustand and use puter for auth functionality it loook so clean and easy

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+
+export const meta = () => [
+  { title: "Jobsy | Auth" },
+  { name: "description", content: "Log into your account" },
+];
 
 const auth = () => {
-  return <div>auth</div>;
+  //here we  are not using regular useState hook because we are using zustand store
+  //to manage the authentication state globally
+  const { isLoading, auth } = usePuterStore();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const next = location.search.split("next=")[1];
+
+  useEffect(() => {
+    if (auth.isAuthenticated) navigate(next);
+  }, [auth.isAuthenticated, next]);
+
+  return (
+    <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
+      <div className="gradient-border shadow-lg">
+        <section className="flex flex-col gap-8 bg-white rounded-2xl p-10">
+          <div className="flex flex-col items-center gap-2 text-center">
+            <h1>Welcome</h1>
+            <h2>Log in To Continue Boost your Career </h2>
+          </div>
+          <div>
+            {isLoading ? (
+              <button className="auth-button animate-pulse">
+                <p>Signing you in...</p>
+              </button>
+            ) : (
+              <>
+                {auth.isAuthenticated ? (
+                  <button className="auth-button" onClick={auth.signOut}>
+                    <p>Log Out</p>
+                  </button>
+                ) : (
+                  <button className="auth-button" onClick={auth.signIn}>
+                    <p>Log In</p>
+                  </button>
+                )}
+              </>
+            )}{" "}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default auth;
