52d303480185f2b1c90132fa1f429c9ef1b968cc
MONU MANDAL <monumandel3@gmail.com>
2025-08-04 21:29:50 +0530

    in the home we loaded the entire resume from the puter store and render it using resume card but earlier we were just setting the imageurl of every resume from store to give card access to iimage path

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,9 +3,8 @@ import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import resume from "./resume";
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,12 +14,31 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  const { auth } = usePuterStore();
+  const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
+  const [resumess, setResumess] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
 
   useEffect(() => {
     if (!auth.isAuthenticated) navigate("/auth?next=/");
   }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResumes(true);
+
+      const resumess = (await kv.list("resume:*", true)) as KVItem[];
+
+      const parsedResumes = resumess?.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+
+      setResumess(parsedResumes || []);
+      setLoadingResumes(false);
+    };
+
+    loadResumes();
+  }, []);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -37,6 +55,28 @@ export default function Home() {
           {resumes.map((resume: Resume) => (
             <ResumeCard key={resume.id} resume={resume} />
           ))}
+        </div>
+      )}
+      {loadingResumes && (
+        <div className="flex flex-col items-center justify-center">
+          <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+        </div>
+      )}
+      {!loadingResumes && resumess.length > 0 && (
+        <div className="resumes-section">
+          {resumess.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+      {!loadingResumes && resumes?.length === 0 && (
+        <div className="flex flex-col items-center justify-center mt-10 gap-4">
+          <Link
+            to="/upload"
+            className="primary-button w-fit text-xl font-semibold"
+          >
+            Upload Resume
+          </Link>
         </div>
       )}
     </main>
