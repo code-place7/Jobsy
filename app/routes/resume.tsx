839217968523d1fbbd35e8161c11d11c539c6fee
MONU MANDAL <monumandel3@gmail.com>
2025-08-04 11:01:54 +0530

    in the mid process of implementing Summary component created another component ScoreGauge

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,6 +1,9 @@
 import { useParams, Link, useNavigate } from "react-router";
 import { usePuterStore } from "~/lib/puter";
 import { useState, useEffect } from "react";
+import Summary from "~/components/Summary";
+import Ats from "~/components/Ats";
+import Details from "~/components/Details";
 
 export const meta = () => [
   { title: "Jobsy | Review " },
@@ -75,9 +78,12 @@ const resume = () => {
           <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
           {feedback ? (
             <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-              {/* <Summary feedback={feedback} />
-                            <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-                            <Details feedback={feedback} /> */}
+              <Summary feedback={feedback} />
+              <Ats
+                score={feedback.ATS.score || 0}
+                suggestions={feedback.ATS.tips || []}
+              />
+              <Details feedback={feedback} />
             </div>
           ) : (
             <img src="/images/resume-scan-2.gif" className="w-full" />
